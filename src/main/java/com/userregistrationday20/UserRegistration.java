bc1f37aff1f4791ab35d229b5ab5bc4a5a5ff842
vismayatk002 <vismayatk002@gmail.com>
2021-08-24 13:00:20 +0530

    Refactor validation of first name

--- src/main/java/com/userregistrationday20/UserRegistration.java
+++ src/main/java/com/userregistrationday20/UserRegistration.java
@@ -1,9 +1,33 @@
 package com.userregistrationday20;
 
+import java.util.Scanner;
+
 public class UserRegistration {
 	
+	public boolean validateFirstName(String firstName) {
+			
+		boolean returnFlag = true;
+		if(Character.isUpperCase(firstName.charAt(0))) {
+			
+			if(firstName.length() < 3) {
+				
+				System.out.println( "First name should have minimum 3 letters");
+				returnFlag = false;
+			}
+		}
+		else {
+			System.out.println( "first letter must be caps");
+			returnFlag = false;
+		}
+		return returnFlag;
+	}
     public static void main( String[] args ) {
-    	
-        System.out.println( "" );
+        
+    	UserRegistration user = new UserRegistration();
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print( "Enter your first name : ");
+    	String firstName = sc.nextLine(); 
+    	user.validateFirstName(firstName);
+    	sc.close();
     }
 }
